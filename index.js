36b48eadf0e8fc6c6343dc6b79c6f91e51195433
Cleboost <clement.balarot@gmail.com>
2024-06-23 17:56:06 +0200

    init

---
+++ index.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const fs = require('fs');
+const cuuid = require('cuuid');
+const multer = require('multer');
+const cors = require('cors');
+const canvas = require('canvas');
+const {loadImage} = require("canvas");
+const {write} = require("fs");
+
+const app = express();
+
+const cacheUpLink = []
+
+const upload = multer({dest: 'uploads/'});
+
+
+app.use(cors());
+app.get('/upload', (req, res) => {
+    const type = req.query.type;
+    const fileName = req.query.fileName;
+    const id = cuuid();
+
+    cacheUpLink.push({id, type, fileName});
+
+    res.send({link: `https://${req.get('host')}/upload/${id}`});
+})
+
+app.post('/upload/:id', upload.single('file'), async (req, res) => {
+    const id = req.params.id;
+    if (!cacheUpLink.find(e => e.id === id)) {
+        return res.sendStatus(404);
+    }
+
+    const type = cacheUpLink.find(e => e.id === id).type;
+    const fileName = cacheUpLink.find(e => e.id === id).fileName;
+    const targetPath = `file/${type}/${fileName}`;
+
+    if (!fs.existsSync(`file/${type}`)) fs.mkdirSync(`file/${type}`);
+
+    const tempPath = req.file.path;
+
+    await fs.renameSync(tempPath, targetPath)
+    postTrait(targetPath);
+    return cacheUpLink.splice(cacheUpLink.findIndex(e => e.id === id), 1);
+});
+
+app.get('/file/:type/:fileName', (req, res) => {
+    const type = req.params.type;
+    const fileName = req.params.fileName;
+
+    const targetPath = `file/${type}/${fileName}.png`;
+
+    const isExist = fs.existsSync(targetPath);
+    if (!isExist) return res.sendFile("default.png", {root: __dirname});
+
+    return res.sendFile(targetPath, {root: __dirname})
+})
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+
+async function postTrait(imgPath) {
+    const canva = canvas.createCanvas(200, 200);
+    const ctx = canva.getContext('2d');
+    ctx.drawImage(await loadImage(imgPath), 0, 0, 200, 200);
+
+    const buffer = canva.toBuffer('image/png');
+    return fs.writeFileSync(imgPath, buffer);
+}
